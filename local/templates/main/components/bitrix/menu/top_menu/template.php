a297ea1707cce152b5cd08371d91e42abc749f68
Роман Карнаухов <WorkKarn@yandex.ru>
2024-05-16 10:16:06 +0700

    интеграция меню

--- local/templates/main/components/bitrix/menu/top_menu/template.php
+++ local/templates/main/components/bitrix/menu/top_menu/template.php
@@ -1,20 +1,10 @@
-<?if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
+<? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die(); ?>
 
-<?if (!empty($arResult)):?>
-<ul class="left-menu">
+<? if (!empty($arResult)) : ?>
 
-<?
-foreach($arResult as $arItem):
-	if($arParams["MAX_LEVEL"] == 1 && $arItem["DEPTH_LEVEL"] > 1) 
-		continue;
-?>
-	<?if($arItem["SELECTED"]):?>
-		<li><a href="<?=$arItem["LINK"]?>" class="selected"><?=$arItem["TEXT"]?></a></li>
-	<?else:?>
-		<li><a href="<?=$arItem["LINK"]?>"><?=$arItem["TEXT"]?></a></li>
-	<?endif?>
-	
-<?endforeach?>
-
-</ul>
-<?endif?>
+	<ul class="header_menu">
+		<? foreach ($arResult as $arItem) : ?>
+			<li><a class="menu_lvl1_bt <?= !empty($arItem["SELECTED"]) ? 'active' : '' ?>" href="<?= $arItem["LINK"] ?>"><span><?= $arItem["TEXT"] ?></span></a></li>
+		<? endforeach ?>
+	</ul>
+<? endif ?>
--- local/templates/main/header.php
+++ local/templates/main/header.php
@@ -20,28 +20,29 @@ if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true)
 	Asset::getInstance()->addCss(SITE_TEMPLATE_PATH . "/assets/css/jquery.bxslider.css");
 	Asset::getInstance()->addCss(SITE_TEMPLATE_PATH . "/assets/css/jquery.formstyler.css");
 	Asset::getInstance()->addCss(SITE_TEMPLATE_PATH . "/assets/css/jquery.formstyler.theme.css");
-	
+
 	Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . "/assets/js/jquery-3.1.1.min.js");
 	Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . "/assets/js/jquery.fancybox.min.js");
 	Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . "/assets/js/jquery.bxslider.min.js");
 	Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . "/assets/js/jquery.formstyler.min.js");
 	Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . "/assets/js/main.js");
-	
+
 	?>
 	<? $APPLICATION->ShowHead(); ?>
 </head>
+
 <body>
-<div id="panel" >
-<?$APPLICATION->ShowPanel();?>
-</div>
-<header>
+	<div id="panel">
+		<? $APPLICATION->ShowPanel(); ?>
+	</div>
+	<header>
 		<div class="top_head_white_bl">
 			<div class="standart_width">
-				<div class="top_head_logo"><a href="/"><img src="<?=SITE_TEMPLATE_PATH ?>/assets/img/logo.png" alt=""/></a></div>
+				<div class="top_head_logo"><a href="/"><img src="<?= SITE_TEMPLATE_PATH ?>/assets/img/logo.png" alt="" /></a></div>
 				<div class="top_head_tel_bl">
 					<div class="top_head_tel">
 						<a href="tel:+79003002010">8 (900) 300-20-10</a>
-						<span>  с 8:00 до 20:00 (Мск)</span>
+						<span> с 8:00 до 20:00 (Мск)</span>
 					</div>
 				</div>
 				<div class="top_head_favor_bl">
@@ -55,99 +56,59 @@ if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true)
 		<div class="blue_head_info_bl">
 			<div class="standart_width clear_after">
 				<div class="header_menu_bl">
-					<ul class="header_menu">
-						<li>
-							<a class="menu_lvl1_bt arrow active" href=""><span>Каталог</span></a>
-							<ul class="menu_lvl2">
-								<li>
-									<a class="menu_lvl2_bt arrow active" href="">Учебная литература</a>
-									<ul class="menu_lvl3">
-										<li><a href="">Для школьников</a></li>
-										<li><a class="active" href="">Дополнительное образование</a></li>
-										<li><a href="">Книги для дошкольников</a></li>
-										<li><a href="">Подготовка к ЕГЭ</a></li>
-										<li><a href="">Подготовка к ОГЭ (ГИА)</a></li>
-										<li><a href="">Словари и разговорники</a></li>
-									</ul>
-								</li>
-								<li>
-									<a class="menu_lvl2_bt arrow" href="">Детская литература</a>
-									<ul class="menu_lvl3">
-										<li><a href="">Для школьников</a></li>
-										<li><a href="">Дополнительное образование</a></li>
-										<li><a href="">Книги для дошкольников</a></li>
-										<li><a href="">Подготовка к ЕГЭ</a></li>
-										<li><a href="">Подготовка к ОГЭ (ГИА)</a></li>
-										<li><a href="">Словари и разговорники</a></li>
-									</ul>
-								</li>
-								<li>
-									<a class="menu_lvl2_bt arrow" href="">Художественная литература</a>
-									<ul class="menu_lvl3">
-										<li><a href="">Для школьников</a></li>
-										<li><a href="">Дополнительное образование</a></li>
-										<li><a href="">Книги для дошкольников</a></li>
-										<li><a href="">Подготовка к ЕГЭ</a></li>
-										<li><a href="">Подготовка к ОГЭ (ГИА)</a></li>
-										<li><a href="">Словари и разговорники</a></li>
-									</ul>
-								</li>
-								<li>
-									<a class="menu_lvl2_bt arrow" href="">Бизнес-литература</a>
-									<ul class="menu_lvl3">
-										<li><a href="">Для школьников</a></li>
-										<li><a href="">Дополнительное образование</a></li>
-										<li><a href="">Книги для дошкольников</a></li>
-										<li><a href="">Подготовка к ЕГЭ</a></li>
-										<li><a href="">Подготовка к ОГЭ (ГИА)</a></li>
-										<li><a href="">Словари и разговорники</a></li>
-									</ul>
-								</li>
-							</ul>
-						</li>
-						<li><a class="menu_lvl1_bt" href=""><span>Акции</span></a></li>
-						<li><a class="menu_lvl1_bt" href=""><span>Доставка</span></a></li>
-						<li><a class="menu_lvl1_bt" href=""><span>О компании</span></a></li>
-						<li><a class="menu_lvl1_bt" href=""><span>Новости</span></a></li>
-						<li><a class="menu_lvl1_bt" href=""><span>Контакты</span></a></li>
-					</ul>
+					<? $APPLICATION->IncludeComponent(
+						"bitrix:menu",
+						"top_menu",
+						array(
+							"ALLOW_MULTI_SELECT" => "N",
+							"CHILD_MENU_TYPE" => "left",
+							"DELAY" => "N",
+							"MAX_LEVEL" => "3",
+							"MENU_CACHE_GET_VARS" => array(0 => "",),
+							"MENU_CACHE_TIME" => "3600",
+							"MENU_CACHE_TYPE" => "N",
+							"MENU_CACHE_USE_GROUPS" => "N",
+							"ROOT_MENU_TYPE" => "top",
+							"USE_EXT" => "Y"
+						)
+					); ?>
 				</div>
 				<div class="head_search_bl">
 					<form>
-						<input type="text" placeholder="Поиск ..."/>
-						<input type="submit" value="найти"/>
+						<input type="text" placeholder="Поиск ..." />
+						<input type="submit" value="найти" />
 					</form>
 				</div>
 			</div>
 		</div>
-		<?if($APPLICATION->GetCurpage() === "/"):?>
-		<div class="index_slider_bl">
-			<ul class="index_slider">
-				<li>
-					<div class="index_slider_el" style="background-image:url(./img/index_slider_bg.png);">
-						<div class="standart_width index_slider_cont">
-							<div class="ind_slid_txt_bl">
-								<div class="ind_slid_tit">Осенний ценопад</div>
-								<div class="ind_slid_txt">Перспективное планирование способствует повышению качества поставленных обществом задач. В частности, синтетическое тестирование прекрасно подходит для реализации стандартных подходов.</div>
-								<a class="red_bt" href="">Узнать подробнее</a>
+		<? if ($APPLICATION->GetCurpage() === "/") : ?>
+			<div class="index_slider_bl">
+				<ul class="index_slider">
+					<li>
+						<div class="index_slider_el" style="background-image:url(./img/index_slider_bg.png);">
+							<div class="standart_width index_slider_cont">
+								<div class="ind_slid_txt_bl">
+									<div class="ind_slid_tit">Осенний ценопад</div>
+									<div class="ind_slid_txt">Перспективное планирование способствует повышению качества поставленных обществом задач. В частности, синтетическое тестирование прекрасно подходит для реализации стандартных подходов.</div>
+									<a class="red_bt" href="">Узнать подробнее</a>
+								</div>
+								<div class="ind_slid_img"><img src="./img/index_slider1.png" alt="" /></div>
 							</div>
-							<div class="ind_slid_img"><img src="./img/index_slider1.png" alt=""/></div>
 						</div>
-					</div>
-				</li>
-				<li>
-					<div class="index_slider_el" style="background-image:url(./img/index_slider_bg.png);">
-						<div class="standart_width index_slider_cont">
-							<div class="ind_slid_txt_bl">
-								<div class="ind_slid_tit">Осенний ценопад</div>
-								<div class="ind_slid_txt">Перспективное планирование способствует повышению качества поставленных обществом задач. В частности, синтетическое тестирование прекрасно подходит для реализации стандартных подходов.</div>
-								<a class="red_bt" href="">Узнать подробнее</a>
+					</li>
+					<li>
+						<div class="index_slider_el" style="background-image:url(./img/index_slider_bg.png);">
+							<div class="standart_width index_slider_cont">
+								<div class="ind_slid_txt_bl">
+									<div class="ind_slid_tit">Осенний ценопад</div>
+									<div class="ind_slid_txt">Перспективное планирование способствует повышению качества поставленных обществом задач. В частности, синтетическое тестирование прекрасно подходит для реализации стандартных подходов.</div>
+									<a class="red_bt" href="">Узнать подробнее</a>
+								</div>
+								<div class="ind_slid_img"><img src="./img/index_slider1.png" alt="" /></div>
 							</div>
-							<div class="ind_slid_img"><img src="./img/index_slider1.png" alt=""/></div>
 						</div>
-					</div>
-				</li>
-			</ul>
-		</div>
-		<?endif?>
+					</li>
+				</ul>
+			</div>
+		<? endif ?>
 	</header>
